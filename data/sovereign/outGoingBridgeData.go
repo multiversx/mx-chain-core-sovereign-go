563eb96298af1c3f2f8f8a58e90021cbc4dbab0c
Marius C <marius.ciucam@gmail.com>
2025-04-17 15:18:38 +0300

    FEAT: Chain ID in outgoing bridge data

--- data/sovereign/outGoingBridgeData.go
+++ data/sovereign/outGoingBridgeData.go
@@ -1,2 +1,2 @@
-//go:generate protoc -I=. --go_out=$GOPATH/src --go-grpc_out=$GOPATH/src outGoingBridgeData.proto
+//go:generate protoc -I=. --go_out=$GOPATH/src --go-grpc_out=$GOPATH/src --gogoslick_out=. --gogoslick_out=paths=source_relative:. outGoingBridgeData.proto
 package sovereign
