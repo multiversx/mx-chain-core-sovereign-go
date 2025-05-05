0dca4103c16fdd6940cbc6ac7218e0099468a361
Marius C <marius.ciucam@gmail.com>
2025-05-05 13:28:08 +0300

    FEAT: Refactor cross chain id to work as shard id

--- data/sovereign/dto/sourceChainID.go
+++ data/sovereign/dto/sourceChainID.go
@@ -1,3 +1,15 @@
 //go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf --gogoslick_out=paths=source_relative:. sourceChainID.proto
 
 package dto
+
+// ValidChains defines supported cross chain ids
+var ValidChains = map[ChainID]struct{}{
+	MVX: {},
+	ETH: {},
+}
+
+// IsCrossChainID returns true if the provided chain id is supported
+func IsCrossChainID(id ChainID) bool {
+	_, ok := ValidChains[id]
+	return ok
+}
